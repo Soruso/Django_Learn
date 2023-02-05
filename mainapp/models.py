3833303ac05d101f365663adf38efbd59fce964e
Soruso <burov.1990@gmail.com>
2023-02-05 13:36:10 +0300

    lesson_4 Fixtures

--- mainapp/models.py
+++ mainapp/models.py
@@ -67,7 +67,7 @@ class CourseTeachers(models.Model):
     course = models.ManyToManyField(Courses)
     name_first = models.CharField(max_length=128, verbose_name="Name")
     name_second = models.CharField(max_length=128, verbose_name="Surname")
-    day_birth = models.DecimalField(max_digits=256, decimal_places=256, verbose_name="Birth date")
+    day_birth = models.CharField(max_length=128, verbose_name="Birth date")
     deleted = models.BooleanField(default=False)
 
     def __str__(self) -> str:
